1ac077dd554282241893a0dc1dd211d5119d44db
James Hurst <james@pintsized.co.uk>
2014-12-10 11:35:33 +0000

    Normalising metatable needs to be created on the fly so that we can have multiple non-overlapping instances.

--- lib/resty/http_headers.lua
+++ lib/resty/http_headers.lua
@@ -9,31 +9,38 @@ local _M = {
     _VERSION = '0.01',
 }
 
-local mt = { 
-    normalised = {}
-}
 
-
-mt.__index = function(t, k)
-    k = str_gsub(str_lower(k), "-", "_")
-    if mt.normalised[k] then
-        return rawget(t, mt.normalised[k])
-    end
-end
-
-
-mt.__newindex = function(t, k, v)
-    local k_low = str_gsub(str_lower(k), "-", "_")
-    if not mt.normalised[k_low] then
-        mt.normalised[k_low] = k
-        rawset(t, k, v)
-    else
-        rawset(t, mt.normalised[k_low], v)
-    end
-end
-
-
+-- Returns an empty headers table with internalised case normalisation. 
+-- Supports the same cases as in ngx_lua:
+--
+-- headers.content_length
+-- headers["content-length"]
+-- headers["Content-Length"]
 function _M.new(self)
+    local mt = { 
+        normalised = {},
+    }
+
+
+    mt.__index = function(t, k)
+        local k_hyphened = str_gsub(k, "_", "-")
+        local matched = rawget(t, k)
+        if matched then
+            return matched
+        else
+            local k_normalised = str_lower(k_hyphened)
+            return rawget(mt.normalised, k_normalised)
+        end
+    end
+
+
+    mt.__newindex = function(t, k, v)
+        local k_hyphened = str_gsub(k, "_", "-")
+        local k_normalised = str_lower(k_hyphened)
+        rawset(mt.normalised, k_normalised, v)
+        rawset(t, k_hyphened, v)
+    end
+
     return setmetatable({}, mt)
 end
 
