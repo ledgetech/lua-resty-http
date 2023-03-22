4ab4269cf442ba52507aa2c718f606054452fcad
James Hurst <james@reel.audio>
2023-03-22 09:45:52 +0000

    Bump version to 0.17.1

--- lib/resty/http.lua
+++ lib/resty/http.lua
@@ -106,7 +106,7 @@ end
 
 
 local _M = {
-    _VERSION = '0.17.0-beta.1',
+    _VERSION = '0.17.1',
 }
 _M._USER_AGENT = "lua-resty-http/" .. _M._VERSION .. " (Lua) ngx_lua/" .. ngx.config.ngx_lua_version
 
